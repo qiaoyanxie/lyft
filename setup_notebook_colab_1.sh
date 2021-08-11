a86d50bf444905ce2aabc05cf7bfe010d1a90a58
qiaoyanxie <87598766+qiaoyanxie@users.noreply.github.com>
2021-08-11 11:43:29 +0200

    Add files via upload

---
+++ setup_notebook_colab_1.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Make a temporary download folder
+TEMP_DOWNLOAD_DIR=$(mktemp -d)
+TEMP_DATASET_DIR=$(mktemp -d)
+
+# Download sample zarr
+echo "Downloading train zarr dataset..."
+wget https://lyft-l5-datasets-public.s3-us-west-2.amazonaws.com/prediction/v1.1/train.tar \
+    -q --show-progress -P $TEMP_DOWNLOAD_DIR
+
+mkdir -p $TEMP_DATASET_DIR/scenes
+tar xf $TEMP_DOWNLOAD_DIR/sample.tar -C $TEMP_DATASET_DIR/scenes
+
+# Download semantic map
+echo "Downloading semantic map..."
+wget https://lyft-l5-datasets-public.s3-us-west-2.amazonaws.com/prediction/v1.1/semantic_map.tar \
+    -q --show-progress -P $TEMP_DOWNLOAD_DIR
+mkdir -p $TEMP_DATASET_DIR/semantic_map
+tar xf $TEMP_DOWNLOAD_DIR/semantic_map.tar -C $TEMP_DATASET_DIR/semantic_map
+cp $TEMP_DATASET_DIR/semantic_map/meta.json $TEMP_DATASET_DIR/meta.json
+
+# Download aerial maps
+echo "Downloading aerial maps (this can take a while)..."
+wget https://lyft-l5-datasets-public.s3-us-west-2.amazonaws.com/prediction/v1.1/aerial_map.tar \
+    -q --show-progress -P $TEMP_DOWNLOAD_DIR
+tar xf $TEMP_DOWNLOAD_DIR/aerial_map.tar -C $TEMP_DATASET_DIR
+
+# Dowload sample configuration
+wget https://github.com/lyft/l5kit/blob/master/examples/agent_motion_prediction/agent_motion_config.yaml -q
+
+# Install L5Kit
+echo "Installing L5kit..."
+pip install --progress-bar off --quiet -U l5kit pyyaml
+
+echo "Dataset and L5kit are ready !"
+echo $TEMP_DATASET_DIR > "dataset_dir.txt"
